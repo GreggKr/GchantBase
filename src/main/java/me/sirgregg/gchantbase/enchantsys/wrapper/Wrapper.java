6632d61b90ff410fadee16b8ee972d911cc7e136
gregg <greggkrzykowski@gmail.com>
2017-09-08 17:58:54 -0500

    Cleaned up

--- src/main/java/me/sirgregg/gchantbase/enchantsys/wrapper/Wrapper.java
+++ src/main/java/me/sirgregg/gchantbase/enchantsys/wrapper/Wrapper.java
@@ -23,18 +23,14 @@ public class Wrapper {
 		List<EnchantWrapper> wrappers = new ArrayList<>();
 		for (Map.Entry<String, BaseEnchant> entry : GchantBase.getEnchantManager().getRegisteredEnchants().entrySet()) {
 			BaseEnchant enchant = entry.getValue();
-			System.out.println("1: " + entry.getValue().getName());
 			if (enchant.hasEnchant(item)) {
 				System.out.println(45);
-				System.out.println("has " + entry.getValue().getName());
 				for (String lore : item.getItemMeta().getLore()) {
 					System.out.println(lore);
 					if (lore.contains(enchant.getName())) {
-						System.out.println("has 2 " + enchant.getName());
 						int level = GchantBase.getRomanNumberalUtil().decode(lore.substring(COLOR_CODE_LENGTH + enchant.getName().length() + 1));
 						EnchantWrapper wrapper = new EnchantWrapper(enchant, level);
 						wrappers.add(wrapper);
-						System.out.println("Added: " + wrapper.getEnchant().getName() + ":" + wrapper.getLevel());
 					}
 				}
 			}
