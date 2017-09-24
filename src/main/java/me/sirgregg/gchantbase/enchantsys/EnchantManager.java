b8129716712604ab785f1192fcbf4c69ddba0686
gregg <greggkrzykowski@gmail.com>
2017-09-24 14:48:43 -0500

    Changed up the logic to increase performance and cut down on redundancy

--- src/main/java/me/sirgregg/gchantbase/enchantsys/EnchantManager.java
+++ src/main/java/me/sirgregg/gchantbase/enchantsys/EnchantManager.java
@@ -1,36 +1,26 @@
 package me.sirgregg.gchantbase.enchantsys;
 
-import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EnchantManager {
 	private HashMap<String, BaseEnchant> registeredEnchants = new HashMap<>();
 
-	public void setupEnchants() {
-	}
+	public int getLevel(ItemStack item, BaseEnchant enchant) {
+		Objects.requireNonNull(enchant);
 
-//	public int getLevel(ItemStack item, BaseEnchant enchant) {
-//		Objects.requireNonNull(item);
-//		Objects.requireNonNull(enchant);
-//
-//		if (!enchant.hasEnchant(item)) return -1;
-//
-//		for (String line : item.getItemMeta().getLore()) {
-//			if (line.startsWith(ChatColor.GRAY + enchant.getName())) {
-//				return GchantBase.getRomanNumberalUtil().decode(line.substring(enchant.getName().length() + 3));
-//			}
-//		}
-//		return -1;
-//	}
+		return enchant.getLevel(item);
+	}
 
 	public void registerEnchant(BaseEnchant enchant) {
 		if (!registeredEnchants.containsKey(enchant.getName())) {
 			registeredEnchants.put(enchant.getName(), enchant);
 		}
+	}
+
+	public void registerEnchants(BaseEnchant... enchants) {
+		Arrays.stream(enchants).forEach(this::registerEnchant);
 	}
 
 	public void unregisterEnchant(BaseEnchant enchant) {
@@ -39,26 +29,24 @@ public class EnchantManager {
 		}
 	}
 
+	public void unregisterEnchants(BaseEnchant... enchants) {
+		Arrays.stream(enchants).forEach(this::unregisterEnchant);
+	}
+
 	public BaseEnchant getEnchant(String name) {
 		if (registeredEnchants.containsKey(name)) return registeredEnchants.get(name);
 		return null;
 	}
 
-	public String[] getAllEnchantNames() {
-		return new String[] {
-				String.valueOf(registeredEnchants.keySet())
-		};
+	public HashSet<String> getAllEnchantNames() {
+		return new HashSet<>(registeredEnchants.keySet());
 	}
 
 	public boolean hasEnchant(BaseEnchant enchant, ItemStack item) {
-		if (item == null || enchant == null) return false;
-		if (!item.hasItemMeta()) return false;
-		if (!item.getItemMeta().hasLore()) return false;
+		Objects.requireNonNull(enchant);
+		Objects.requireNonNull(item);
 
-		for (String string : item.getItemMeta().getLore()) {
-			if (string.startsWith(enchant.getColor() + enchant.getName())) return true;
-		}
-		return false;
+		return enchant.hasEnchant(item);
 	}
 
 	public Map<String, BaseEnchant> getRegisteredEnchants() {
