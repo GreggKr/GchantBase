c1d3fe136c7a1309096cf2f9809ca179730d066b
Gregg <greggkrzykowski@gmail.com>
2018-06-12 17:49:03 -0500

    no more wrapper

--- src/main/java/me/sirgregg/gchantbase/GchantBase.java
+++ src/main/java/me/sirgregg/gchantbase/GchantBase.java
@@ -2,19 +2,16 @@ package me.sirgregg.gchantbase;
 
 import me.sirgregg.gchantbase.command.GchantCommand;
 import me.sirgregg.gchantbase.enchantsys.EnchantManager;
-import me.sirgregg.gchantbase.enchantsys.wrapper.Wrapper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GchantBase extends JavaPlugin {
     private static GchantBase instance;
-    private static Wrapper wrapper;
     private static EnchantManager enchantManager;
 
     @Override
     public void onEnable() {
         instance = this;
         enchantManager = new EnchantManager();
-        wrapper = new Wrapper();
 
         //registerConfig();
         registerCommands();
@@ -26,10 +23,6 @@ public class GchantBase extends JavaPlugin {
 
     private void registerCommands() {
         getCommand("gchant").setExecutor(new GchantCommand());
-    }
-
-    public static Wrapper getWrapper() {
-        return wrapper;
     }
 
     public static GchantBase getInstance() {
